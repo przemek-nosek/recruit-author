9e3a405f21cd2726f5d95a0cbf3f7a494812eac6
Przemek Nosek <przemek.nosek@o2.pl>
2021-08-25 08:59:13 +0200

    added book entity

--- src/main/java/pl/java/recruitauthor/entity/Book.java
+++ src/main/java/pl/java/recruitauthor/entity/Book.java
@@ -5,6 +5,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "books")
@@ -15,4 +19,35 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @NotEmpty
+    @Column(nullable = false)
+    private String Isbn; // TODO: add ISBN validation
+
+    @NotNull
+    @NotEmpty
+    @Column(nullable = false)
+    private String title;
+
+    @ManyToMany(mappedBy = "books")
+    private Set<Author> authors = new HashSet<>();
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Category category;
+
+    public Book(String isbn, String title, Set<Author> authors, Category category) {
+        Isbn = isbn;
+        this.title = title;
+        this.authors = authors;
+        this.category = category;
+    }
+
+    public void addAuthor(Author author) {
+        this.authors.add(author);
+    }
+
+    public void removeAuthor(Author author) {
+        this.authors.remove(author);
+    }
 }
---
+++ src/main/java/pl/java/recruitauthor/entity/Category.java
@@ -0,0 +1,18 @@
+package pl.java.recruitauthor.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
