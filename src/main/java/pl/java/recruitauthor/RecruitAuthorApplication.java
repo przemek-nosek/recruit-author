a61efc2af3919bba8ebd84572d19069c44247452
Przemek Nosek <przemek.nosek@o2.pl>
2021-08-25 08:32:28 +0200

    Initial commit

---
+++ src/main/java/pl/java/recruitauthor/RecruitAuthorApplication.java
@@ -0,0 +1,13 @@
+package pl.java.recruitauthor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitAuthorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecruitAuthorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/java/recruitauthor/RecruitAuthorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.java.recruitauthor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitAuthorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
