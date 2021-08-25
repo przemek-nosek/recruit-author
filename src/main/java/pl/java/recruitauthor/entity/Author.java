88fd208a4eb36e2e294b843a7171c3752ed0cd13
Przemek Nosek <przemek.nosek@o2.pl>
2021-08-25 08:47:33 +0200

    created Author entity and added validation

---
+++ src/main/java/pl/java/recruitauthor/entity/Author.java
@@ -0,0 +1,54 @@
+package pl.java.recruitauthor.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "authors")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Author {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    @Column(nullable = false, length = 40)
+    private String firstName;
+
+    @NotNull
+    @NotBlank
+    @Column(nullable = false, length = 40)
+    private String lastName;
+
+    @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinTable(
+            name = "authors_books",
+            joinColumns = @JoinColumn(name = "id"),
+            inverseJoinColumns = @JoinColumn(name="id")
+    )
+    private Set<Book> books = new HashSet<>();
+
+    public Author(String firstName, String lastName, Set<Book> books) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = books;
+    }
+
+    public void addBook(Book book) {
+        this.books.add(book);
+    }
+
+    public void removeBook(Book book) {
+        this.books.remove(book);
+    }
+}
